75b82453d8eb8b6d770aa32e506d484154c4138c
RodrigoCury <rodrigocury@ufu.br>
2021-01-28 21:12:33 -0300

    Added Allignment Model to ORM database

--- biop/model.py
+++ biop/model.py
@@ -17,3 +17,18 @@ class Sequence_model(db.Model):
 
     def __str__(self):
         return f"sequencia {self.seq_id} ; {self.seq_type} ; {self.sequence} ; {self.translation_table} ; {self.creationDate}"
+
+
+class Alignment_model(db.Model):
+    align_id = db.Column(db.Integer, primary_key=True)
+    seq1 = db.Column(db.String, nullable=False)
+    seq2 = db.Column(db.String, nullable=False)
+    alignment_type = db.Column(db.String, nullable=False)
+    creationDate = db.Column(
+        db.DateTime, nullable=False, defalt=datetime.utcnow)
+
+    def __str__(self):
+        return f"ID: {self.align_id}; \n 1st Sequence: {self.seq1} \n ; 2nd Sequence: {self.seq2}; \n"
+
+    def __repr__(self):
+        return f"ID: {self.align_id}; \n 1st Sequence: {self.seq1} \n ; 2nd Sequence: {self.seq2}; \n"
